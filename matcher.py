60874957c20096adc2b6196387554f7d1685744f
Urvisha Jivani <urvishasheta@gmail.com>
2025-06-10 13:47:21 -0400

    Imported required Libraries

---
+++ matcher.py
@@ -0,0 +1,11 @@
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+# Download NLTK data if not already downloaded
+nltk.download('punkt')
+nltk.download('stopwords')
+
